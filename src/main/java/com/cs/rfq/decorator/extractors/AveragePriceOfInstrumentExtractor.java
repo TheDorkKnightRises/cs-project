9abc6fa6d2e15442bc6fd61fa39d92417a26d982
Nikhil <nikhilpowar16@gmail.com>
2019-07-17 01:53:39 -0400

    completed story 5

--- src/main/java/com/cs/rfq/decorator/extractors/AveragePriceOfInstrumentExtractor.java
+++ src/main/java/com/cs/rfq/decorator/extractors/AveragePriceOfInstrumentExtractor.java
@@ -6,23 +6,39 @@ import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 import org.joda.time.DateTime;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class AveragePriceOfInstrumentExtractor implements RfqMetadataExtractor {
 
+    private String since;
+
+    public AveragePriceOfInstrumentExtractor () {
+        this.since = new DateTime().minusWeeks(1).toString();
+    }
+
     @Override
     public Map<RfqMetadataFieldNames, Object> extractMetaData(Rfq rfq, SparkSession session, Dataset<Row> trades) {
 
-        long todayMs = DateTime.now().withMillisOfDay(0).getMillis();
-        long pastWeekMs = DateTime.now().withMillis(todayMs).minusWeeks(1).getMillis();
+        String query = String.format("SELECT avg(LastPx) from trade where SecurityId='%s' AND TradeDate >= '%s'",
+                rfq.getIsin(),
+                since);
 
-        Dataset<Row> filtered = trades
-                .filter(trades.col("SecurityId").equalTo(rfq.getIsin()))
-                .filter(trades.col("TradeDate").$greater(new java.sql.Date(pastWeekMs)));
+        trades.createOrReplaceTempView("trade");
+        Dataset<Row> queryResult = session.sql(query);
 
-        double avgPrice = filtered.agg();
-        return null;
+        Object averagePrice = queryResult.first().get(0);
+
+        if (averagePrice == null) {
+            averagePrice = 0;
+        }
+
+        Map<RfqMetadataFieldNames, Object> results = new HashMap<>();
+        results.put(RfqMetadataFieldNames.averageInstrumentPrice, averagePrice);
+        return results;
     }
 
-
+    protected void setSince (String since) {
+        this.since = since;
+    }
 }
---
+++ src/test/java/com/cs/rfq/decorator/extractors/AveragePriceOfInstrumentExtractorTest.java
@@ -0,0 +1,59 @@
+package com.cs.rfq.decorator.extractors;
+
+import com.cs.rfq.decorator.Rfq;
+import com.cs.rfq.decorator.TradeDataLoader;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.joda.time.DateTime;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+
+public class AveragePriceOfInstrumentExtractorTest extends AbstractSparkUnitTest {
+    private Rfq rfq;
+
+    @Before
+    public void setup() {
+        rfq = new Rfq();
+        rfq.setIsin("AT0000A0VRQ6");
+    }
+
+    @Test
+    public void checkAveragePriceWhenInstrumentMatch() {
+
+        String filePath = getClass().getResource("volume-traded-1.json").getPath();
+        Dataset<Row> trades = new TradeDataLoader().loadTrades(session, filePath);
+
+        AveragePriceOfInstrumentExtractor extractor = new AveragePriceOfInstrumentExtractor();
+        extractor.setSince("2018-06-10");
+
+        Map<RfqMetadataFieldNames, Object> meta = extractor.extractMetaData(rfq, session, trades);
+
+        Object result = meta.get(RfqMetadataFieldNames.averageInstrumentPrice);
+
+        assertEquals(125.977, result);
+//        assertEquals(138.4396, result);
+    }
+
+    @Test
+    public void checkAveragePriceWhenNoInstrumentMatch() {
+
+        String filePath = getClass().getResource("volume-traded-1.json").getPath();
+        Dataset<Row> trades = new TradeDataLoader().loadTrades(session, filePath);
+
+        //all test trade data are for 2018 so this will cause no matches
+        AveragePriceOfInstrumentExtractor extractor = new AveragePriceOfInstrumentExtractor();
+        extractor.setSince("2019-06-17");
+
+        Map<RfqMetadataFieldNames, Object> meta = extractor.extractMetaData(rfq, session, trades);
+
+        Object result = meta.get(RfqMetadataFieldNames.averageInstrumentPrice);
+
+        assertEquals(0, result);
+
+    }
+}
